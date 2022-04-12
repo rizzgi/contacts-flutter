617f251a9d6411db5f736de9a873c41519238a6b
giovanni rizzardo <giovannirizzardo@MacBook-Pro-de-giovanni.local>
2022-04-12 00:53:38 -0300

    inserindo helper na aplicacao

---
+++ app/lib/ui/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:app/helpers/contact_helper.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  ContactHelper helper = ContactHelper();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Contact c = Contact();
+    // c.name = "giovanni";
+    // c.email = "gkoefe@fenfe";
+    // c.phone = "22222222";
+    // c.img =  'fefefe';
+    //
+    // helper.saveContact(c);
+
+    helper.getAllContacts().then((list) {
+      print(list);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
